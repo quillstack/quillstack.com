ade8a1ccc5e0f92618bda6e779d1e196cd37f8f7
Radek Ziemniewicz <radoslaw@ziemniewicz.pl>
2020-09-26 22:50:31 +0100

    Changed logo to smaller SVG version

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,15 @@
 <template>
     <div>
-        <div class="border-b-2 w-full border-solid pb-4" style="border-color: #2c2c2c;">
+        <div class="border-b w-full border-solid" style="border-color: #2c2c2c;">
             <div class="w-11/12 md:w-1/3 lg:w-1/4 pl-2 mx-auto">
                 <nuxt-link to="/">
-                    <img src="~/assets/top-logo.png" alt="QuillStack" class="mx-auto"/>
+                    <object type="image/svg+xml"
+                            width="100"
+                            id="logo"
+                            :data="require('@/assets/quillstack-simple.svg')"
+                            class="mx-auto pointer-events-none">
+                        Quill Stack
+                    </object>
                 </nuxt-link>
             </div>
         </div>
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,12 +2,28 @@ import webpack from 'webpack';
 
 export default {
     build: {
+        dir: 'docs',
         plugins: [
             new webpack.ProvidePlugin({
                 // global modules
                 _: 'lodash'
             }),
         ]
+    },
+    redirect: [
+        {
+            // eslint-disable-next-line
+            from: '(?!^\/$|^\/[?].*$)(.*\/[?](.*)$|.*\/$)',
+            to: (from, req) => {
+                const base = req._parsedUrl.pathname.replace(/\/$/, '');  // We take pathname instead of req.url because of the query parameters
+                const search = req._parsedUrl.search;
+                return base + (search != null ? search : '');
+            },
+            statusCode: 301
+        },
+    ],
+    router: {
+        trailingSlash: false
     },
     generate: {
         dir: 'docs'
@@ -17,6 +33,7 @@ export default {
     ],
     modules: [
         'vue-scrollto/nuxt',
+        '@nuxtjs/redirect-module',
     ],
     css: [
         '@/assets/css/style.css',
